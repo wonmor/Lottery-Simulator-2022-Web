def3d300b400d361e9a3551155d74b5f882ed516
John Seong <wonmor@gmail.com>
2022-03-03 02:43:14 -0500

    Updated folder structure

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
-web: gunicorn --chdir WEB-VER-ALPHA main:app
+web: gunicorn main:app
 
 # HOW TO SET UP HEROKU GUNICRON PROFILE: https://stackoverflow.com/questions/38851564/heroku-gunicorn-procfile
--- website/__init__.py
+++ website/__init__.py
@@ -16,9 +16,7 @@ def create_app():
 
     # Load the config
     app.config.update(
-        TESTING=True,
         SECRET_KEY='192b9bdd22ab9ed4d12e236c78afcb9a393ec15f71bbf5dc987d54727823bcbf',
-        DEBUG=True
     )
 
     # Add SQLAlchemy database
