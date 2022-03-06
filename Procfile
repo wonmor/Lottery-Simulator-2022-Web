8c0c78bf57f335ea99b318f39628cb8589275396
John Seong <wonmor@gmail.com>
2022-03-06 14:27:12 -0500

    Update

--- Procfile
+++
@@ -1,3 +0,0 @@
-web: gunicorn main:app
-
-# HOW TO SET UP HEROKU GUNICRON PROFILE: https://stackoverflow.com/questions/38851564/heroku-gunicorn-procfile
--- website/extensions.py
+++
@@ -1,5 +0,0 @@
-# SQLAlchemy is designed to acommodate developers with a easy-to-use database model
-from flask_sqlalchemy import SQLAlchemy
-
-# Set up the database
-db = SQLAlchemy()
